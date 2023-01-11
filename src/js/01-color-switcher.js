720c865fde1f13ae7f7c634af3f355360c726ec0
MaAlina1708 <alinamastega@outlook.com>
2023-01-11 17:47:49 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const COLOR_DELAY = 1000;
+let intervalId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, COLOR_DELAY);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+const 
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
